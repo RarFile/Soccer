715017571bfd243a65c2ce651de13140e7bfb2bb
RarFile <43934405+RarFile@users.noreply.github.com>
2023-05-23 12:51:22 -0500

    Initial Commit

--- src/soccer/League.java
+++ src/soccer/League.java
@@ -51,6 +51,23 @@ public class League {
 //        for (Player thePlayer : team2.playerArray) {
 //            System.out.println(thePlayer.playerName);
 //        }
+
+    Game currGame = new Game();
+    currGame.homeTeam = team1;
+    currGame.awayTeam = team2;
+    
+    Goal goal1 = new Goal();
+    goal1.thePlayer = currGame.homeTeam.playerArray[2];
+    goal1.theTeam = currGame.homeTeam;
+    goal1.theTime = 55;
+    
+    Goal[] theGoals = {goal1};
+    currGame.goals = theGoals;
+    
+    System.out.println("Goal scored after " +
+    currGame.goals[0].theTime + " mins by " +
+    currGame.goals[0].thePlayer.playerName + " of " +
+    currGame.goals[0].theTeam.teamName);
     }
     
 }
